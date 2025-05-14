347abb5c86c12b1d4decc174175ee1e24fbc84a3
nikomain <nikolai.main@youlend.com>
2025-05-14 11:01:47 +0100

    test th

--- th/th-functions.sh
+++ th/th-functions.sh
@@ -2,7 +2,7 @@
 # 		    Teleport CLI shortcuts
 # ===========================================================
 th(){
-  echo 'p3rwien"
+  echo "krejer"
   th_login() {
     if tsh status 2>/dev/null | grep -q 'Logged in as:'; then
       echo "Already logged in to Teleport."
